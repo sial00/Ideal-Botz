4c7227274b5803138ed98f97123167a74afbd0f0
sial00 <appcreator39@gmail.com>
2025-07-29 09:01:45 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const {
   const { File } = require('megajs')
   const prefix = '.'
   
-  const ownerNumber = ['923191776595']
+  const ownerNumber = ['923237045919']
   
   //===================SESSION-AUTH============================
   if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
@@ -67,22 +67,22 @@ const {
   }
   });
   console.log('Plugins installed successful ✅')
-  console.log('Ideal-Botz connected to whatsapp ✅')
+  console.log('your name connected to whatsapp ✅')
   
   let up =`╔═════════════════╗
-  ║      Ideal-Botz           
+  ║      your name           
   ║  SUCCESSFULLY CONNECTED ✅ 😍        
   ╠═════════════════╣
   ║      • PREFIX: .            
   ╟─────────────────╢
   ║ ♻ 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝐋𝐈𝐍𝐊         
-  ║ https://whatsapp.com/channel/0029Vax8CdP5PO14xesUeU3t              
+  ║ https://whatsapp.com/channel/0029VarfjW04tRrmwfb8x306              
   ╟─────────────────╢
   ║ ♻ 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 𝐋𝐈𝐍𝐊          
-  ║ https://wa.me/message/3191776595                 
+  ║ https://wa.me/message/VRZ5QLDAHXKSF1                 
   ╠═════════════════╣
   ║      your name               
-  ║ > © 亗★🇦brar ||🇸Iᗩᒪ★亗       
+  ║ > © ᴘᴏᴡᴇʀᴇᴅ ʙʏ your name       
   ╚═════════════════╝
 `;
   conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://files.catbox.moe/atby2t.png` }, caption: up })
